2edfa3ec03c2d8f2207d8ee841c55be6458ed980
pepomontero10 <jmontero3@miuandes.cl>
2021-04-19 20:31:28 -0400

    commit

--- printer.py
+++ printer.py
@@ -1,7 +1,29 @@
- class Print:
 
-     def menu(self, Jugador):
-         print(Jugador "Que le gustaria hacaer?")
-         print("1. Income")
-         print("2. ")
 
+
+class Printer:
+
+    def menu(self, numero):
+        print("Player", numero,  ",what would you like to do?")
+        print("1. Income"
+            ,"2. Foreign Aid"
+            ,"3. Coup"
+            ,"4. Tax"
+            ,"5. Addassinate"
+            ,"6. Exchange"
+            ,"7. Steal"
+            , sep="\n")
+        return input()
+        
+    def challenge(self, Jugador):
+        print("Does someone want to Challenge?")
+        print("Yes, then identify your self (Your number):")
+        print("No, then enter 0")
+        return input()
+
+    def counterattack(self, Jugador):
+        print("Does someone want to counterattack?")
+        print("Yes, then identify your self (Your number):")
+        print("No, then enter 0")
+        return input()
+        
