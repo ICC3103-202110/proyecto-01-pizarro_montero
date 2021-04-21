2f87c7b68257763cfbc94dcdea5a7db3e40b6090
pepomontero10 <jmontero3@miuandes.cl>
2021-04-21 14:19:21 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -6,47 +6,49 @@ class Action:
         # self.action_name = ''
         # self.action = ''
         self.coinsGained = 0
+        self.coinsNeeded = 0
 
     def play_action(self):
         return self.coinsGained
 
-class Income(Action):
-    def __init__(self):
-        self.coinsGained = 1
+    def Income(self):
+        return self.coinsGained = 1
         print('listo')
 
-class ForeignAid(Action):
-    def __init__(self):
-        self.coinsGained = 2
+    def ForeignAid(self):
+        return self.coinsGained = 2
 
 
-class Coup(Action):
-    def __init__(self):
-        self.coinsGained = -7
+    def Coup(Action):
+        return self.coinsGained = -7
 
     
-
-# class Duke(self):
-#     def tax(self):
-#         #obtiene 3 monedas
+    def Duke(self):
+        return self.coinsGained = 3
+        #obtiene 3 monedas
 #         self.coinsGained = 3
         
     
 
-# class Assasin(self):
-#     def assessinate(self, target):
-#         self.coinsNeeded = -3
-#         #paga 3 mmonedas y asesina una influencia
-#         #Players.loseInfluence(target)
+    def Assasin(self):
+        return self.coinsNeeded = -3
+        
+         #paga 3 mmonedas y asesina una influencia
+         #Players.loseInfluence(target)
     
-
-# class Captain(self):
-#     def steel(self):
+    def Captain(self):
+        self.coinsGained = 2
+        self.coinsNeeded = -2
+        x = self.coinsGained
+        y = self.coinsNeeded
+        z = []
+        z.append(x)
+        z.append(y)
+        # lista con las monedas que robo y las monedas que se le tiene que quitar 
+        return z      
 #         #quita max 2 monedas
-#         pass
-#     pass
 
-# class Ambassador(self):
+    def Ambassador(self):
 #     def exchange(self):
 #         #cambia cartas
 #         pass
