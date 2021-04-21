c600871fef9e1d8436445758ca907c610345125e
pepomontero10 <jmontero3@miuandes.cl>
2021-04-21 17:00:56 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -16,7 +16,7 @@ class Action:
 
     def Coup(Action):
         self.coinsGained = -7
-        target = input("A quien te gustaria quitarle una influencia?")
+        target = input("A quien te gustaria quitarle una influencia?:")
         coins = self.coinsGained
         coup = []
         coup.append(target)
@@ -27,35 +27,42 @@ class Action:
     
     def Duke(self):
         self.coinsGained = 3
-        coins = c
-        #obtiene 3 monedas
-#         self.coinsGained = 3
+        coins = self.coinsGained
+        person = input("Who do you want to take coins from?:")
+        duke = []
+        duke.append(person)
+        duke.append(coins)
+        return duke
+        #lista que tiene a la persona que se le quita las monedas y la cantidad de monedas
         
     
 
     def Assasin(self):
-        self.coinsNeeded = -3
-        
+        self.coinsGained = -3
+        coins = self.coinsGained
+        person = input("Who do you want to kill?:")
+        assasin = []
+        assasin.append(person)
+        assasin.append(coins)
+        return assasin
          #paga 3 mmonedas y asesina una influencia
          #Players.loseInfluence(target)
     
     def Captain(self):
         self.coinsGained = 2
-        self.coinsNeeded = -2
-        x = self.coinsGained
-        y = self.coinsNeeded
-        z = []
-        z.append(x)
-        z.append(y)
-        # lista con las monedas que robo y las monedas que se le tiene que quitar 
-        return z      
-#         #quita max 2 monedas
+        coins = self.coinsGained
+        person = input("From whom would you like to steal?:")
+        captain = []
+        captain.append(person)
+        captain.append(coins)
+        return captain 
+
 
     def Ambassador(self):
-#     def exchange(self):
-#         #cambia cartas
-#         pass
-#     pass
+        card = input("Which card would you like to change?:")
+        person = input("Which player do you want to change with?:")
+        ambassador = []
+        ambassador.append(card)
+        ambassador.append(person)
+        return ambassador
 
-# class Contessa(self):
-#     pass
